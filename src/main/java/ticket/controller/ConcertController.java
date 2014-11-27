84f5c14dcbbfb252654a2701fbb6a17c958156cf
Bappel <Bappel@Bappels-MacBook-Pro-2.local>
2014-11-27 01:41:08 +0100

    nå funker noe

--- src/main/java/ticket/controller/ConcertController.java
+++ src/main/java/ticket/controller/ConcertController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
+
 import ticket.model.Concert;
+import ticket.repositories.ArtistRepository;
 import ticket.repositories.ConcertRepository;
 
 @ManagedBean
@@ -14,30 +16,71 @@ import ticket.repositories.ConcertRepository;
 public class ConcertController {
 	
 	List<Concert> concerts = new ArrayList<Concert>();
+	Concert concert = new Concert();
 	
 	@Inject
 	private ConcertRepository repository;
-	Concert concert = new Concert();
+	@Inject
+	private ArtistRepository artistRepository;
 	
+	private int artistId;
+	private int concertId;
+	private int reservedTickets;
+	
+	public int getReservedTickets() {
+		return reservedTickets;
+	}
+	public void setReservedTickets(int reservedTickets) {
+		this.reservedTickets = reservedTickets;
+	}
+
+	private String searchString;
+	
+	public String getSearchString() {
+		return searchString;
+	}
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+	
+	public void updateTickets(){
+		this.concert = concerts.get(concertId);
+		this.concert.setTicketsleft(concert.getTicketstotal() - reservedTickets);
+		repository.update(this.concert);
+	}
+	
+	public int getConcertId() {
+		return concertId;
+	}
+	public void setConcertId(int concertId) {
+		this.concertId = concertId;
+	}
+	public int getArtistId() {
+		return artistId;
+	}
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
 	public Concert getConcert() {
 		return concert;
 	}
 	public void addConcert() {	
+		concert.setArtist(artistRepository.findOne(artistId));
 		repository.add(this.concert);
 	}
 	public void deleteConcert(){
-		repository.delete(this.concert.getId());
+		repository.delete(concertId);
 	}
 	public List<Concert> getConcerts(){
 		concerts = repository.findAll();
 		return concerts;
 	}
 
-    public Concert getInput() {
-        return concert;
+    public Concert getInput(){
+        return repository.findOne(concertId);
     }
 
-    public void setInput(Concert concert) {
-        this.concert = concert;
+    public void setInput(int id) {
+        this.concertId = id;
     }
 }
